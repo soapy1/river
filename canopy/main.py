cf46c4b3095c2c53c7741d867f66ad5e0bf80ac1
sophia <scastellarin@quansight.com>
2025-02-26 12:44:16 -0800

    Save all checkpoints to target path

--- canopy/main.py
+++ canopy/main.py
@@ -4,6 +4,7 @@ import datetime
 import yaml
 
 from canopy.park import Park
+from canopy.data_dir import DataDir
 
 
 PARK_URL = os.environ.get("PARK_URL")
@@ -28,7 +29,7 @@ def inspect_park_checkpoints(namespaces: List[str]) -> List[str]:
                 save_checkpoint = {
                         "created": env_timestamp,
                         "fqn": f"{namespace}/{env}/{chk}",
-                        "checkpoint": chk
+                        "checkpoint": env_chk,
                     }
 
                 current_latest_checkpoint = checkpoints.get(f"{namespace}/{env}")
@@ -45,24 +46,24 @@ def inspect_park_checkpoints(namespaces: List[str]) -> List[str]:
     return checkpoints
 
 
-def save_checkpoints_to_volume(checkpoints, path):
-    park_api = Park(url=PARK_URL)
-
-    checkpoint_files_path = f"{path}/checkpoints/"
-    os.makedirs(checkpoint_files_path, exist_ok=True)
+def save_checkpoints(checkpoints, path):
+    data_dir = DataDir(data_dir=path)
     for namespace_env, checkpoint_dict in checkpoints.items():
-        latest_checkpoint = checkpoint_dict["latest"]["checkpoint"]
-        print(f"latest checkpoint for {namespace_env} is {latest_checkpoint}")
-        checkpoint_file = f"{checkpoint_files_path}/{namespace_env}/{latest_checkpoint}"
-        # see if latest checkpoint already exists
-        if os.path.exists(checkpoint_file):
-            print(f"checkpoint file already exists at {checkpoint_file}, skipping")
-        else:
-            os.makedirs(f"{checkpoint_files_path}/{namespace_env}", exist_ok=True)
-            latest_checkpoint = park_api.get_checkpoint(namespace_env.split("/")[0], namespace_env.split("/")[1], latest_checkpoint)
-            with open(checkpoint_file, "w+") as file:
-                yaml.dump(latest_checkpoint.model_dump(), file)
+        latest_uuid = checkpoint_dict["latest"]["checkpoint"].uuid
+        # save all the checkpoints
+        for checkpoint in checkpoint_dict["checkpoints"]:
+            latest = False
+            if checkpoint["checkpoint"].uuid == latest_uuid:
+                latest = True
+            print(f"saving checkpoint {checkpoint['fqn']}! latest: {latest}")
 
+            # HACK: major hacks. clean this up.
+            data_dir.save_environment_checkpoint(
+                checkpoint=checkpoint["checkpoint"],
+                namespace=namespace_env.split("/")[0],
+                env=namespace_env.split("/")[1],
+                latest=latest,
+            ) 
 
 def sync():
     WATCHED_NAMESPACES = os.environ.get("WATCHED_NAMESPACES")
@@ -71,14 +72,17 @@ def sync():
     else:
         WATCHED_NAMESPACES = WATCHED_NAMESPACES.split(",")
 
-    target_path = os.environ.get("TARGET_PATH")
-    if target_path is None:
-        target_path = "/tmp/canopy"
+    # get path that canopy writes to
+    canopy_path = os.environ.get("TARGET_PATH")
+    if canopy_path is None:
+        canopy_path = "/tmp/canopy"
+
+    # checkpoints get saved to the `checkpoint` dir inside the canopy_path
+    target_path = f"{canopy_path}/checkpoints"
 
     # get checkpoints for all the watched namespaces
     checkpoints = inspect_park_checkpoints(WATCHED_NAMESPACES)
-    print(f"checkpoints: {checkpoints}")
-    save_checkpoints_to_volume(checkpoints, target_path)
+    save_checkpoints(checkpoints, target_path)
 
 
 if __name__ == "__main__":
